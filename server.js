63f21828843f9966e86d4494985fd0d912133719
VaishaliTech <vaishalitech.info@gmail.com>
2026-02-16 20:46:56 +0530

    CHENGING PORT

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ import bookingRoutes from './routes/bookingRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
