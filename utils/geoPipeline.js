b254a3465007e33227adf20b3f3c0a73df66769d
deepakkumar1211 <sahudeepak1211@gmail.com>
2026-03-06 23:07:49 +0530

    feat: refactor getSalonByID and geoNearStage for improved distance calculation and flexibility

--- utils/geoPipeline.js
+++ utils/geoPipeline.js
@@ -1,18 +1,44 @@
-export const geoNearStage = ({ lat, lng, radius = 5 }) => {
+
+// export const geoNearStage = ({ lat, lng, radius = 25 }) => { // radius in km
+//   // if (!lat || !lng) return [];
+//   if (lat == null || lng == null) return [];
+
+//   return [
+//     {
+//       $geoNear: {
+//         near: {
+//           type: "Point",
+//           coordinates: [parseFloat(lng), parseFloat(lat)],
+//         },
+//         distanceField: "distanceInMeters",
+//         maxDistance: parseFloat(radius) * 1000,
+//         spherical: true,
+//       },
+//     },
+//   ];
+// };
+
+
+// radius in km, if radius is not provided then it will return all nearby salons sorted by distance
+export const geoNearStage = ({ lat, lng, radius }) => {
   // if (!lat || !lng) return [];
   if (lat == null || lng == null) return [];
 
-  return [
-    {
-      $geoNear: {
-        near: {
-          type: "Point",
-          coordinates: [parseFloat(lng), parseFloat(lat)],
-        },
-        distanceField: "distanceInMeters",
-        maxDistance: parseFloat(radius) * 1000,
-        spherical: true,
+  const geoNear = {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [parseFloat(lng), parseFloat(lat)],
       },
+      distanceField: "distanceInMeters",
+      spherical: true,
     },
-  ];
+  };
+
+  // if radius pass then only add maxDistance
+  if (radius) {
+    geoNear.$geoNear.maxDistance = parseFloat(radius) * 1000;
+  }
+
+  return [geoNear];
 };
