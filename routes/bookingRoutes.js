982731e188c0e858ceaef71d5b93acb22a3f4f17
deepakkumar1211 <sahudeepak1211@gmail.com>
2026-03-16 19:19:29 +0530

    feat: add booking management features for providers; implement endpoints to fetch, accept, and reject bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,13 +3,44 @@ import {
   createBooking,
   getBookingBySalonId,
   getBookingByUserId,
+  getBookingsByProvider,
+  acceptBookingByProvider,
+  rejectBookingByProvider,
 } from "../controllers/bookingController.js";
-import { authenticate } from "../middlewares/authMiddleware.js";
+import { authenticate, authorizeRoles } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/create-booking", authenticate, createBooking);
 router.get("/get-my-bookings", authenticate, getBookingByUserId);
 router.get("/get-salon-bookings", authenticate, getBookingBySalonId);
+
+/*
+  Salon Owner + Independent Professional
+  → Get their bookings
+*/
+router.get(
+  "/provider",
+  authenticate,
+  authorizeRoles("salon_owner", "independent_pro"),
+  getBookingsByProvider
+);
+
+// Salon Owner + Independent Professional → Accept a pending booking and generate service code
+router.patch(
+  "/provider/accept-booking/:bookingId",
+  authenticate,
+  authorizeRoles("salon_owner", "independent_professional"),
+  acceptBookingByProvider
+);
+
+
+// Salon Owner + Independent Professional → Reject a pending booking with cancellation reason
+router.patch(
+  "/provider/reject-booking/:bookingId",
+  authenticate,
+  authorizeRoles("salon_owner", "independent_professional"),
+  rejectBookingByProvider
+);
 
 export default router;
